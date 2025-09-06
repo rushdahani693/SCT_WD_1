14b2570dfc7530897efa68ce7926a8c8e859bc15
rushdahani693 <rushdahani693@gmail.com>
2025-09-06 17:06:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const mainNav = document.getElementById('main-nav');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        mainNav.classList.add('scrolled');
+    } else {
+        mainNav.classList.remove('scrolled');
+    }
+});
